882c2b7bb98968d52415d48f250e92f07d97dbbd
MyraRU <myraviev.ivan@gmail.com>
2022-02-20 20:32:16 +0300

    Person 1.0

---
+++ src/Lesson3/NormalL3/Person.java
@@ -0,0 +1,22 @@
+package Lesson3.NormalL3;
+
+public class Person {
+
+    String FirstMiddleLastName;
+    String post;
+    String email;
+    long phoneNumber;
+    int payCheck;
+    byte age;
+
+    void info() {
+        System.out.println("Ф.И.О. - " + FirstMiddleLastName + ", Должность: " + post + ", Email: "
+        + email + ", Телефон: " + phoneNumber + ", Заралпта: " + payCheck + ", Возраст: " + age);
+    }
+
+
+
+
+
+
+}
