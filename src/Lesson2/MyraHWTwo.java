847a3545bd15fe9f2fa4ac2d151bf7cdc389917c
MyraRU <myraviev.ivan@gmail.com>
2022-02-20 17:17:01 +0300

    first commit

---
+++ src/Lesson2/MyraHWTwo.java
@@ -0,0 +1,38 @@
+package Lesson2;
+
+import java.util.Scanner;
+
+public class MyraHWTwo {
+    public static void main(String[] args) {
+
+        // Задача 1. Калькулятор факториала
+//        Scanner sc = new Scanner(System.in);
+////        System.out.println("Введите факториал");
+////        int n = sc.nextInt();
+////        int b = 1;
+////        for (int a = 1; a <= n; a++) {
+////            b = b * a;
+////        }
+////        System.out.println("Факториал числа " + n + "! = " + b);
+
+        // Задача 2. Фибоначчи
+        int[] array = new int[100];
+        array[0] = 0;
+        array[1] = 1;
+        for (int i = 2; i < array.length; i++){
+            array[i] = array[i - 1] + array[i - 2];
+            System.out.println(array[i]);
+        }
+
+        // Задача 3. Определение регистра и латиницы <-- Не работает.
+//        Scanner sc = new Scanner(System.in);
+//        String str = sc.nextLine();
+//
+//        String[] arr = {"a", "b", "s", "d"};
+//
+//        for (int i = 0; i < arr.length; i++) {
+//            if (str.equals(arr[i]))
+//                System.out.println("da");
+//        }
+    }
+}
---
+++ src/Lesson3/MyraHWThree.java
@@ -0,0 +1,9 @@
+package Lesson3;
+
+public class MyraHWThree {
+    public static void main(String[] args) {
+
+        System.out.println("Hello");
+
+    }
+}
