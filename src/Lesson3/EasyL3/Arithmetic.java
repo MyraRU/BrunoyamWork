90b1a3bdec69412540835e5e5c8a1afc4b89bcbd
MyraRU <myraviev.ivan@gmail.com>
2022-02-20 23:38:45 +0300

    lesson3 HARD 1.0

--- src/Lesson3/EasyL3/Arithmetic.java
+++ src/Lesson3/EasyL3/Arithmetic.java
@@ -3,13 +3,13 @@ package Lesson3.EasyL3;
 public class Arithmetic {
 
     public void setX(int X) {
-        if (X <= 0) {
+        if (X == 0) {
             System.out.println("Вы должны ввести значение больше 0");
         }
     }
 
     public void setY(int Y) {
-        if (Y <= 0) {
+        if (Y == 0) {
             System.out.println("Вы должны ввести значение больше 0");
         }
     }
